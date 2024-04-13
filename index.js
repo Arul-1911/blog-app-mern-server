85d4c598cc9a65c5bb8d474600085857fd3fa622
Arul-1911 <arulpraveen19112002@gmail.com>
2024-04-13 17:14:50 +0530

    connected to db

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express = require('express');
-const app = express()
+const app = express();
+const dotenv = require('dotenv').config();
+const mongoose = require('mongoose')
+
+const dbConnect = async () => {
+   try {
+      await mongoose.connect(process.env.ATLAS_URI);
+      console.log('DB connected succesfully')
+   } catch (error) {
+      console.log(error)
+   }
+}
+dbConnect()
 
 app.listen('8080', () => {
-   console.log('server started')
+   console.log('server started at port 8080')
 })
