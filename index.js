1ddb94e3b777bbe25be6cdc77399cf0a47c98f33
Arul-1911 <arulpraveen19112002@gmail.com>
2024-04-13 20:46:34 +0530

    user crendtials added

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv').config();
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
+//ROUTE IMPORTS
+const authRoute = require('./routes/auth.js');
+const userRoute = require('./routes/users.js')
+
+//MIDDLEWARES
+app.use(express.json())
+
+
+//CONNECTION DB
 const dbConnect = async () => {
    try {
       await mongoose.connect(process.env.ATLAS_URI);
@@ -11,8 +20,15 @@ const dbConnect = async () => {
       console.log(error)
    }
 }
-dbConnect()
+dbConnect(); //calling db conection function
 
+//USAGE OF ROUTES
+app.use('/api/auth', authRoute);
+app.use('/api/users', userRoute);
+
+
+
+//LISTENING PORT
 app.listen('8080', () => {
    console.log('server started at port 8080')
 })
